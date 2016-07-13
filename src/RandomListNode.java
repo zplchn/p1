768b6d0238f7cf2b09d2b0584ff04139e24242a5
zplchn <zplchn@gmail.com>
2016-07-12 22:20:19 -0400

    check in 7/12

---
+++ src/RandomListNode.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zplchn on 7/12/16.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next, random;
+
+    public RandomListNode(int x){
+        this.label = x;
+    }
+}
